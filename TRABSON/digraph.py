24149379dc4058311b113a361def59ac6858de15
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2016-12-11 00:40:49 -0200

    1.1.1

--- TRABSON/digraph.py
+++ TRABSON/digraph.py
@@ -259,9 +259,6 @@ class Digraph(object):
             SONUMEROS = re.findall(r'\d+', linha)
             node = int (SONUMEROS[0])
             SONUMEROS.pop(0)
-            print(node,end = ' ')
-            print(SONUMEROS)
-            time.sleep(1)
             self.addEdges(node, SONUMEROS)
         File.close()
         return self
